5e07df0e2266cf066e7bf803d6711c9a4e6fbc68
Wildan Bimantoro <wildan.bimantoro@aitindo.com>
2017-10-22 22:55:50 +0700

    Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,46 +1,26 @@
-var mongoose = require('mongoose');
-var mongodbUri = "mongodb://localhost:27017/TodoApp";
-var options = {
-	useMongoClient: true,
-	socketTimeoutMS: 0,
-	keepAlive: true,
-	reconnectTries: 30
-};
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(mongodbUri,options);
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo',{
-	text: {
-		type: String
-	},
-	completed: {
-		type: Boolean
-	},
-	completedAt: {
-		type: Number
-	}
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+	var todo = new Todo({
+		"text": req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
 });
 
-// var newTodo = new Todo({
-// 	text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-// 	console.log('Saved todo', doc);
-// },(e) => {
-// 	console.log('Unable to save todo')
-// });
-
-var wildTodo = new Todo({
-	text: 'Shutdown games',
-	completed: true,
-	completedAt: 123
-});
-
-wildTodo.save().then((content) => {
-	// console.log('saved wildtodo',content);
-	console.log(JSON.stringify(content, undefined, 2));
-},(e) => {
-	console.log('Unable to save todo');
+app.listen(3000, () => {
+	console.log('Started on port 3000');
 });
