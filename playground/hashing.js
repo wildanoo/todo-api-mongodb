109b21e429627ae9221c59599e032258342a5082
Wildan Bimantoro <wildan.bimantoro@aitindo.com>
2017-10-30 00:15:29 +0700

    add JWT

---
+++ playground/hashing.js
@@ -0,0 +1,41 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+	id : 10
+};
+
+// jwt.sign
+var token = jwt.sign(data,'123abc');
+console.log('Isi token : ', token);
+
+// jwt.verify
+var decoded = jwt.verify(token,'123abc');
+console.log('Hasil decoded : ',decoded);
+
+
+// var message = "Saya user nomer 3";
+// var hash = SHA256(message).toString();
+
+// console.log(`Message : ${message}`);
+// console.log(`Hash : ${hash}`);
+
+// var data = {
+// 	id: 4
+// }
+
+// var token = {
+// 	data,
+// 	hash : SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// token.data.id = 5;
+// token.data.hash = SHA256(JSON.stringify(data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if( resultHash === token.hash){
+// 	console.log('Data was not changed');
+// } else {
+// 	console.log('Data was changed. Do not trust');
+// }
