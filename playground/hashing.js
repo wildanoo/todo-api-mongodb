dcfe2916f984a00dd4800ef65360a5b23f60a2b0
Wildan Bimantoro <wildan.bimantoro@aitindo.com>
2017-11-05 22:32:24 +0700

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id : 10
-};
+var password = 'abc123!';
 
-// jwt.sign
-var token = jwt.sign(data,'123abc');
-console.log('Isi token : ', token);
+bcrypt.genSalt(10,(err, salt) => {
+	bcrypt.hash(password, salt ,(err,hash) => {
+		console.log(hash);
+	});
+});
 
-// jwt.verify
-var decoded = jwt.verify(token,'123abc');
-console.log('Hasil decoded : ',decoded);
+var hashedPassword = '$2a$10$Kj1otJ7Wq9i7HBKApLQTZuuC/4oxknl7fBNFW2DZAXaS0gWjLHNKy';
+
+bcrypt.compare(password,hashedPassword,(err,res) => {
+	console.log(res);
+});
+
+
+// var data = {
+// 	id : 10
+// };
+
+// // jwt.sign
+// var token = jwt.sign(data,'123abc');
+// console.log('Isi token : ', token);
+
+// // jwt.verify
+// var decoded = jwt.verify(token,'123abc');
+// console.log('Hasil decoded : ',decoded);
 
 
 // var message = "Saya user nomer 3";
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -67,9 +68,26 @@ UserSchema.statics.findByToken = function(token){
 		'tokens.token' : token,
 		'tokens.access' : 'auth'
 	})
-
 };
 
+UserSchema.pre('save',function(next) {
+	var user = this;
+
+	if (user.isModified('password')){
+		bcrypt.genSalt(10,(err,salt) => {
+			bcrypt.hash(user.password, salt, (err,hash) => {
+				console.log('isi user : ', user);
+				console.log('isi user.password : ', user.password);
+				user.password = hash;
+
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+
+});
 
 
 var User = mongoose.model('user', UserSchema);
